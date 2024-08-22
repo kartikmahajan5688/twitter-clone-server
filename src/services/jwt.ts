d921b1adbd8039160147ba69b1f43fac981eb8b5
resolutpartners <104563047+resolutpartners@users.noreply.github.com>
2024-08-22 12:54:56 +0530

    Init commit

---
+++ src/services/jwt.ts
@@ -0,0 +1,18 @@
+import { User } from "@prisma/client";
+import { prismaClient } from "../clients/db";
+import JWT from "jsonwebtoken";
+
+const JWT_SECRET = "Ktm@6508";
+
+class JWTService {
+  public static generateTokenForUser(user: User) {
+    const payload = {
+      id: user?.id,
+      email: user?.email,
+    };
+    const token = JWT.sign(payload, JWT_SECRET);
+    return token
+  }
+}
+
+export default JWTService;
